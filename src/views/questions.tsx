dd5e18acdab7c797ac1a1f3b587491e8c7571430
Dinesh <dinesh@mahaswami.com>
2026-01-22 16:04:24 +0530

    Questions show tweaks

--- src/views/questions.tsx
+++ src/views/questions.tsx
@@ -1,13 +1,32 @@
-import { Resource, createDefaults, tableDefaults, 
-	editDefaults, formDefaults, listDefaults, 
-	showDefaults, RowActions, CardGrid,
-	createReferenceField,
-	createReferenceInput, RichTextField, RichTextInput, ReferenceLiveFilter, ChoicesLiveFilter, NumberLiveFilter, BooleanLiveFilter, TextLiveFilter, recordRep  } from '@mahaswami/swan-frontend';
+import {
+    BooleanLiveFilter,
+    CardGrid,
+    ChoicesLiveFilter, NumberLiveFilter,
+    ReferenceLiveFilter,
+    Resource,
+    RowActions,
+    TextLiveFilter,
+    createDefaults,
+    createReferenceField,
+    createReferenceInput,
+    editDefaults, formDefaults, listDefaults,
+    recordRep,
+    showDefaults,
+    tableDefaults
+} from '@mahaswami/swan-frontend';
 import { Quiz } from '@mui/icons-material';
-import { Box, CardContent, CardHeader } from '@mui/material';
-import { Create, DataTable, Edit, List, Menu, Show, SimpleForm, SimpleShowLayout, 
-    TextField, TextInput, type ListProps, BooleanField, BooleanInput, NumberField, NumberInput, SelectField, SelectInput, AutocompleteInput, required } from "react-admin";
+import {
+    AutocompleteInput,
+    BooleanField, BooleanInput,
+    Create, DataTable, Edit, FunctionField, List, Menu,
+    NumberField, NumberInput, SelectField, SelectInput,
+    Show, SimpleForm, SimpleShowLayout,
+    TextField, TextInput,
+    required,
+    type ListProps
+} from "react-admin";
 import { ConceptsReferenceField, ConceptsReferenceInput } from './concepts';
+import { RenderStream } from './DiagnosticTestRound';
 
 export const RESOURCE = "questions"
 export const ICON = Quiz
@@ -15,28 +34,27 @@ export const PREFETCH: string[] = ["concepts"]
 
 export const QuestionsReferenceField = createReferenceField(RESOURCE, PREFETCH);
 export const QuestionsReferenceInput = createReferenceInput(RESOURCE, PREFETCH);
-export const questionTypeChoices = [{ id: 'mcq', name: 'MCQ' }, { id: '2_marks', name: '2 Marks' }, { id: '4_marks', name: '4 Marks' }, { id: '5_marks', name: '5 Marks' }];
-export const difficultyLevelChoices = [{ id: 'easy', name: 'Easy' }, { id: 'medium', name: 'Medium' }, { id: 'hard', name: 'Hard' }];
+export const difficultyChoices = [{ id: 'easy', name: 'Easy' }, { id: 'medium', name: 'Medium' }, { id: 'hard', name: 'Hard' }];
 
 const filters = [
-    <TextLiveFilter source="search" fields={["option_a", "option_b", "option_c"]} />,
-    <ReferenceLiveFilter source="concept_id" reference="concepts" label="Concept" />,
-    <ChoicesLiveFilter source="question_type" label="Question Type" choiceLabels={questionTypeChoices} />,
-    <ChoicesLiveFilter source="difficulty_level" label="Difficulty Level" choiceLabels={difficultyLevelChoices} />,
+    <TextLiveFilter source="search" show fields={["type", "options", "correct_option"]} />,
+    <ReferenceLiveFilter source="concept_id" show reference="concepts" label="Concept" />,
+    <ChoicesLiveFilter source="difficulty" label="Difficulty" choiceLabels={difficultyChoices} />,
     <NumberLiveFilter source="marks_number" label="Marks" />,
-    <BooleanLiveFilter source="is_active" label="Active" />
+    <BooleanLiveFilter source="is_active" show label="Active" />,
+    <BooleanLiveFilter source="is_invented" label="Invented" />
 ]
 
 export const QuestionsList = (props: ListProps) => {
     return (
         <List {...listDefaults(props)}>
             <DataTable {...tableDefaults(RESOURCE)}>
-                <DataTable.Col source="concept_id" field={ConceptsReferenceField}/>
-                <DataTable.Col source="question_type" field={(props: any) => <SelectField {...props} choices={questionTypeChoices} />}/>
-                <DataTable.Col source="difficulty_level" field={(props: any) => <SelectField {...props} choices={difficultyLevelChoices} />}/>
-                <DataTable.Col source="option_a" />
-                <DataTable.Col source="option_b" />
-                <RowActions/>
+                <DataTable.Col source="concept_id" field={ConceptsReferenceField} />
+                <DataTable.Col source="type" />
+                <DataTable.Col source="difficulty" field={(props: any) => <SelectField {...props} choices={difficultyChoices} />} />
+                <DataTable.Col source="options" />
+                <DataTable.Col source="correct_option" />
+                <RowActions />
             </DataTable>
         </List>
     )
@@ -47,8 +65,8 @@ export const QuestionsCardGrid = (props: ListProps) => {
     return (
         <List {...listDefaults(props)} component={'div'}>
             <CardGrid title={<ConceptsReferenceField source="concept_id" variant='h6' />}>
-                <SelectField source="question_type" choices={questionTypeChoices} />
-                <SelectField source="difficulty_level" choices={difficultyLevelChoices} />
+                <TextField source="type" />
+                <SelectField source="difficulty" choices={difficultyChoices} />
             </CardGrid>
         </List>
     )
@@ -63,18 +81,17 @@ const QuestionForm = (props: any) => {
             <ConceptsReferenceInput source="concept_id">
                 <AutocompleteInput validate={required()} />
             </ConceptsReferenceInput>
-            <SelectInput source="question_type" choices={questionTypeChoices} validate={required()} />
-            <SelectInput source="difficulty_level" choices={difficultyLevelChoices} />
-            <RichTextInput source="question_html" validate={required()} />
-            <TextInput source="option_a" />
-            <TextInput source="option_b" />
-            <TextInput source="option_c" />
-            <TextInput source="option_d" />
-            <TextInput source="correct_answer" />
-            <RichTextInput source="hint_html" />
-            <RichTextInput source="solution_html" />
-            <NumberInput source="marks_number" validate={required()} />
+            <TextInput source="type" validate={required()} />
+            <SelectInput source="difficulty" choices={difficultyChoices} />
+            <TextInput source="options" />
+            <TextInput source="correct_option" />
+            <TextInput source="question_stream" />
+            <TextInput source="hint" />
+            <TextInput source="answer_stream" />
+            <TextInput source="final_answer" />
+            <NumberInput source="marks_number" />
             <BooleanInput source="is_active" />
+            <BooleanInput source="is_invented" />
         </SimpleForm>
     )
 }
@@ -89,68 +106,73 @@ const QuestionEdit = (props: any) => {
 
 const QuestionCreate = (props: any) => {
     return (
-    	<Create {...createDefaults(props)}>
+        <Create {...createDefaults(props)}>
             <QuestionForm />
         </Create>
     )
 }
 
 const QuestionShow = (props: any) => {
-    
+
     return (
         <Show {...showDefaults(props)}>
-            <SimpleShowLayout
-                display="grid"
-                gridTemplateColumns={{ xs: '1fr', md: '1fr 1fr' }}
-                gap="1rem">
+            <SimpleShowLayout>
+                <FunctionField source="question_stream"
+                    render={record => <RenderStream
+                        stream={JSON.parse(record.question_stream)}
+                    />}
+                />
+                <FunctionField source="answer_stream"
+                    render={record => <RenderStream
+                        stream={JSON.parse(record.answer_stream)}
+                    />}
+                />
                 <ConceptsReferenceField source="concept_id" />
-                <SelectField source="question_type" choices={questionTypeChoices} />
-                <SelectField source="difficulty_level" choices={difficultyLevelChoices} />
-                <RichTextField source="question_html" />
-                <TextField source="option_a" />
-                <TextField source="option_b" />
-                <TextField source="option_c" />
-                <TextField source="option_d" />
-                <TextField source="correct_answer" />
-                <RichTextField source="hint_html" />
-                <RichTextField source="solution_html" />
+                <TextField source="type" />
+                <SelectField source="difficulty" choices={difficultyChoices} />
+                <TextField source="options" />
+                <TextField source="correct_option" />
+                <TextField source="hint" />
+                <TextField source="final_answer" />
                 <NumberField source="marks_number" />
                 <BooleanField source="is_active" />
+                <BooleanField source="is_invented" />
             </SimpleShowLayout>
         </Show>
     )
 }
 
 
-export const QuestionsResource =  (
+export const QuestionsResource = (
     <Resource
         name={RESOURCE}
         icon={ICON}
         prefetch={PREFETCH}
-        recordRepresentation={(record: any) => record.type + " - " + recordRep('concepts', record.concept)}
+        recordRepresentation={(record: any) => recordRep('concepts', record.concept)}
         fieldSchema={{
             concept_id: { required: true, resource: 'concepts' },
-            question_type: { type: 'choice', ui: 'select', required: true, choices: questionTypeChoices },
-            difficulty_level: { type: 'choice', ui: 'select', choices: difficultyLevelChoices },
-            question_html: { ui: 'rich', required: true },
-            option_a: {},
-            option_b: {},
-            option_c: {},
-            option_d: {},
-            correct_answer: {},
-            hint_html: { ui: 'rich' },
-            solution_html: { ui: 'rich' },
-            marks_number: { required: true },
-            is_active: {}
+            type: { required: true },
+            difficulty: { type: 'choice', ui: 'select', choices: difficultyChoices },
+            options: {},
+            correct_option: {},
+            question_stream: {},
+            hint: {},
+            answer_stream: {},
+            final_answer: {},
+            marks_number: {},
+            is_active: {},
+            is_invented: {}
         }}
         filters={filters}
-        list={<QuestionsList/>}
-        create={<QuestionCreate/>}
-        edit={<QuestionEdit/>}
-        show={<QuestionShow/>}
-        hasDialog
+        hasColumnChooser
+        list={<QuestionsList />}
+        create={<QuestionCreate />}
+        edit={<QuestionEdit />}
+        show={<QuestionShow />}
         hasLiveUpdate
-        // {{SWAN:RESOURCE_OPTIONS}}
+        hasDialog
+        filtersPlacement='top'
+    // {{SWAN:RESOURCE_OPTIONS}}
     />
 )
 export const QuestionsMenu = () => (
